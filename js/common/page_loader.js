79bfe63211178619fbc01bd3a34fcac15c427ab7
Anro Swart <tainaraon123@gmail.com>
2017-08-29 16:25:01 +0200

    Anro - General update

---
+++ js/common/page_loader.js
@@ -0,0 +1,27 @@
+var pageLoader = function()
+{
+	console.log("Page loader linked");
+
+	function load(role)
+	{
+		switch(role)
+		{
+			
+		}
+	}
+
+	function loadHTML(pageNames)
+	{
+
+	}
+
+	function loadCSS(cssNames)
+	{
+
+	}
+
+	function loadJS(jsNames)
+	{
+
+	}
+};
