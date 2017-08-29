cb3ee75c123f900a90a2dd7537d9e997969af2d5
Tiaan Gerber <tiaangerberdev@gmail.com>
2017-08-29 16:48:34 +0200

    Tiaan - GenUpdate1

--- js/common/page_loader.js
+++ js/common/page_loader.js
@@ -2,26 +2,55 @@ var pageLoader = function()
 {
 	console.log("Page loader linked");
 
-	function load(role)
-	{
-		switch(role)
-		{
-			
+	function load(role) {
+	
+		for(var i = 0; i < assetReferences.length; i++) {
+
+			if(assetReferences[i].role == role) {
+
+				loadAssets(assetReferences[i]);
+				return;
+			}
 		}
+
+		alert("ERROR pageLoader");
 	}
 
-	function loadHTML(pageNames)
+	function loadAssets(asset) {
+
+		loadHtml(asset.htmlFiles);
+		loadCss(asset.jsFiles);
+		loadJs(asset.cssFiles);
+	}
+
+	function loadHtml(htmlFiles)
 	{
 
 	}
 
-	function loadCSS(cssNames)
+	function loadJs(jsFiles)
 	{
 
 	}
 
-	function loadJS(jsNames)
+	function loadCss(cssFiles)
 	{
 
 	}
+
+	var assetReferences = 
+	[
+		{
+			"role":"broker",
+			"cssFiles":["b1.css","b2.css","b3.css"],
+			"jsFiles":["b1.js","b2.js"],
+			"htmlFiles":[]
+		},
+		{
+			"role":"client",
+			"cssFiles":["c1.css","c2.css","c3.css"],
+			"jsFiles":["c1.js","c2.js"],
+			"htmlFiles":[]
+		}
+	];
 };
