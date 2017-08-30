436bee8e916d67813e85cd52d97f9aece1731b2e
Anro Swart <tainaraon123@gmail.com>
2017-08-30 14:14:46 +0200

    Anro - General update

--- js/common/main.js
+++ js/common/main.js
@@ -1,6 +1,7 @@
 (function(){        
     console.log("Main linked");
     var loader = pageLoader();
+    
 
     var myCom = httpCommunicator();
     var nInvoker = userInvoker(myCom);
--- js/common/page_loader.js
+++ js/common/page_loader.js
@@ -3,7 +3,7 @@ var pageLoader = function()
 	console.log("Page loader linked");
 
 	function load(role) {
-	
+	debugger;
 		for(var i = 0; i < assetReferences.length; i++) {
 
 			if(assetReferences[i].role == role) {
@@ -25,8 +25,19 @@ var pageLoader = function()
 
 	function loadHtml(htmlFiles)
 	{
-		//document.getElementById("content").innerHTML='<object type="text/html" data="home.html" ></object>';
-		
+		if(assetReferences.navbar != "")
+		{
+			document.getElementById("navbar_container").innerHTML='<object type="text/html" data="'+ htmlFiles.navbar +'" ></object>';
+		}
+		else
+		{
+			// Load the icon into the nav
+			var img = document.createElement('div');
+			img.style.cssText = 'width:100%, height:100%, background:"img/agrihost.png"';
+			document.getElementById("navbar_container").innerHTML=img;
+		}
+
+		document.getElementById("container").innerHTML='<object type="text/html" data="'+htmlFiles.landing_page+'" ></object>';
 	}
 
 	function loadJs(jsFiles)
@@ -56,13 +67,28 @@ var pageLoader = function()
 			"role":"broker",
 			"cssFiles":["b1.css","b2.css","b3.css"],
 			"jsFiles":["b1.js","b2.js"],
-			"htmlFiles":[]
+			"htmlFiles":{
+				"navbar":"",
+				"landing_page":""
+			}
 		},
 		{
 			"role":"client",
 			"cssFiles":["c1.css","c2.css","c3.css"],
 			"jsFiles":["c1.js","c2.js"],
-			"htmlFiles":[]
+			"htmlFiles":{
+				"navbar":"",
+				"landing_page":""
+			}
+		},
+		{
+			"role":"",
+			"cssFiles":["css/common/login.css"],
+			"jsFiles":["login.js"],
+			"htmlFiles":{
+				"navbar":"",
+				"landing_page":"html/common/login.html"
+			}
 		}
 	];
 };
