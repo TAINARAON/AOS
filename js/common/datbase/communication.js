987428d15bba528564d71747915acbcd312ed156
Anro Swart <tainaraon123@gmail.com>
2017-08-30 11:02:23 +0200

    Anro - General update, adding of database communication

---
+++ js/common/datbase/communication.js
@@ -0,0 +1,22 @@
+var communication = function()
+{
+	function request(requestType, url, requestData, callback)
+	{
+		var async = true; // Otherwise program will hang
+
+		var request = new XMLHttpRequest();
+
+		request.onload = function () {
+			var status = request.status; // HTTP response status, e.g., 200 for "200 OK"
+			var data = request.responseText; // Returned data, e.g., an HTML document.
+
+			callback(status, data);
+		}
+
+		request.open(method, url, async);
+
+		request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+		//request.setRequestHeader("Content-Type", "text/plain;charset=UTF-8");
+		request.send(postData);
+	}
+}
