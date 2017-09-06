5af8eea2c7eeea2d02a8586137515b7b62c43e56
Anro Swart <tainaraon123@gmail.com>
2017-09-06 11:48:13 +0200

    Anro - Code cleanup

--- js/broker/quote/quote.js
+++ js/broker/quote/quote.js
@@ -22,48 +22,68 @@
 
 	function createQuoteAccordion(container, quoteData) {
 		for (let i = 0; i < quoteData.length; i++) {
-			console.log("Creating elements: " + i);
-			var button = document.createElement('BUTTON');
-			button.className = "accordion";
-			console.log("Title: " + quoteData[i].title);
-			button.innerHTML = quoteData[i].title;
-
-			var detailContainer = document.createElement('DIV');
-			detailContainer.className = "panel";
-
-			var reQuote = document.createElement('BUTTON');
-			reQuote.innerHTML = "Re-quote";
-			var deleteQuote = document.createElement('BUTTON');
-			deleteQuote.innerHTML = "Delete Quote";
-			var acceptQuote = document.createElement('BUTTON');
-			acceptQuote.innerHTML = "Accept Quote";
-			var printQuote = document.createElement('BUTTON');
-			printQuote.innerHTML = "Print Quote";
-			var emailQuote = document.createElement('BUTTON');
-			emailQuote.innerHTML = "Email Quote";
-
-			var details = document.createElement('P');
-			details.innerHTML = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
-
-			detailContainer.appendChild(reQuote);
-			detailContainer.appendChild(deleteQuote);
-			detailContainer.appendChild(acceptQuote);
-			detailContainer.appendChild(printQuote);
-			detailContainer.appendChild(emailQuote);
-
-			detailContainer.appendChild(details);
-
-			container.appendChild(button);
-			container.appendChild(detailContainer);
-
-			reQuote.onclick = function(e){reQuoteClick(i);};
-			deleteQuote.onclick = function(e){deleteQuoteClick(i);};
-			acceptQuote.onclick = function(e){acceptQuoteClick(i);};
-			printQuote.onclick = function(e){printQuoteClick(i);};
-			emailQuote.onclick = function(e){emailQuoteClick(i);};
-
-			button.onclick = function(e){accordionItemClick(e);};
+			createAccordionParentElement(container, quoteData, i);
+			createChildDetailContainer(container, quoteData, i);
 		}
+  	}
+
+  	function createAccordionParentElement(container, quoteData, index)
+  	{
+  		var button = document.createElement('BUTTON');
+		button.className = "accordion";
+		console.log("Title: " + quoteData[index].title);
+		button.innerHTML = quoteData[index].title;
+
+		container.appendChild(button);
+
+		button.onclick = function(e){accordionItemClick(e);};
+
+		return button;
+  	}
+
+  	function createChildDetailContainer(container, quoteData, index)
+  	{
+  		var detailContainer = document.createElement('DIV');
+		detailContainer.className = "panel";
+
+		container.appendChild(detailContainer);
+
+		createQuoteRelatedChildHeaderButtons(detailContainer, index);
+		createChild(detailContainer);
+  	}
+
+  	function createQuoteRelatedChildHeaderButtons(detailContainer, index)
+  	{
+  		var reQuote = document.createElement('BUTTON');
+		reQuote.innerHTML = "Re-quote";
+		var deleteQuote = document.createElement('BUTTON');
+		deleteQuote.innerHTML = "Delete Quote";
+		var acceptQuote = document.createElement('BUTTON');
+		acceptQuote.innerHTML = "Accept Quote";
+		var printQuote = document.createElement('BUTTON');
+		printQuote.innerHTML = "Print Quote";
+		var emailQuote = document.createElement('BUTTON');
+		emailQuote.innerHTML = "Email Quote";
+
+		detailContainer.appendChild(reQuote);
+		detailContainer.appendChild(deleteQuote);
+		detailContainer.appendChild(acceptQuote);
+		detailContainer.appendChild(printQuote);
+		detailContainer.appendChild(emailQuote);
+
+		reQuote.onclick = function(e){reQuoteClick(index);};
+		deleteQuote.onclick = function(e){deleteQuoteClick(index);};
+		acceptQuote.onclick = function(e){acceptQuoteClick(index);};
+		printQuote.onclick = function(e){printQuoteClick(index);};
+		emailQuote.onclick = function(e){emailQuoteClick(index);};
+  	}
+
+  	function createChild(detailContainer)
+  	{
+  		var details = document.createElement('P');
+		details.innerHTML = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+  		
+  		detailContainer.appendChild(details);
   	}
 
   	function reQuoteClick(id)
