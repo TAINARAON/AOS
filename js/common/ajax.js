ca46ecce777b1b972999c4b2682be973c8a5bd54
Tiaan Gerber <tiaangerberdev@gmail.com>
2017-10-11 12:25:53 +0200

    Tiaan - trying to get ajax call to work

--- js/common/ajax.js
+++ js/common/ajax.js
@@ -25,40 +25,30 @@ function ajaxGet(url,successCallback,failureCallback,mockResponse) {
 
 function ajaxPost(url,successCallback,failureCallback,requestData,mockResponse) {
 
-<<<<<<< HEAD
-	/*console.log("AJAXPost URL:");
-=======
-	console.log("AJAXPost URL:");
->>>>>>> 11ba175347e00003611f5054a5d11b6768c96bc6
-	console.log(url);
-	console.log("AJAXPost requestData:");
-	console.log(requestData);
-	console.log("AJAXPost mockResponse:");
-	console.log(mockResponse);
+	if(mockResponse != null) {
+		console.warn("NOT REAL RESPONSE");
+		successCallback(mockResponse);
+	} else {
 
-	successCallback(mockResponse);*/
-	var async = true;
+		var async = true;
 
-	var request = new XMLHttpRequest();
-	request.onLoad = function() {
-		var status = request.status; // HTTP response status, e.g., 200 for "200 OK"
-		var data = request.responseText; // Returned data, e.g., an HTML document.
+		var request = new XMLHttpRequest();
+		request.onLoad = function() {
+			var status = request.status; // HTTP response status, e.g., 200 for "200 OK"
+			var data = request.responseText; // Returned data, e.g., an HTML document.
 
-		if(status == 200)
-		{
-			successCallback(data);
+			if(status == 200)
+			{
+				successCallback(data);
+			}
+			else
+			{
+				failureCallback(data);
+			}
 		}
-		else
-		{
-			failureCallback(data);
-		}
+		
+		request.open("POST", url, async);
+		request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+		request.send(requestData);
 	}
-	
-	request.open("POST", url, async);
-	request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-<<<<<<< HEAD
-	request.send(requestData);
-=======
-	request.send(requestData);*/
->>>>>>> 11ba175347e00003611f5054a5d11b6768c96bc6
 }
