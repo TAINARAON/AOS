a0f2966b1817b6ca1e415ff2cf397222f3576e51
Tiaan Gerber <tiaangerberdev@gmail.com>
2017-10-16 19:51:50 +0200

    Tiaan - after rebase

--- js/common/ajax.js
+++ js/common/ajax.js
@@ -1,54 +1,71 @@
 function ajaxGet(url,successCallback,failureCallback,mockResponse) {
 
 	successCallback(mockResponse);
-	/*var async = true;
-
-	var request = new XMLHttpRequest();
-	request.onLoad = function() {
-		var status = request.status; // HTTP response status, e.g., 200 for "200 OK"
-		var data = request.responseText; // Returned data, e.g., an HTML document.
-
-		if(status == 200)
-		{
-			successCallback(data);
-		}
-		else
-		{
-			failureCallback(data);
-		}
-	}
-	
-	request.open("GET", url, async);
-	request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-	request.send();*/
-}
-
-function ajaxPost(url,successCallback,failureCallback,requestData,mockResponse) {
-
 	if(mockResponse != null) {
-		//console.warn("NOT REAL RESPONSE");
+		console.warn("NOT REAL RESPONSE");
 		successCallback(mockResponse);
 	} else {
 
 		var async = true;
 
 		var request = new XMLHttpRequest();
-		request.onLoad = function() {
-			var status = request.status; // HTTP response status, e.g., 200 for "200 OK"
-			var data = request.responseText; // Returned data, e.g., an HTML document.
+		request.onreadystatechange  = function() {
 
-			if(status == 200)
+			if (request.readyState === 4)
 			{
-				successCallback(data);
+				var status = request.status; // HTTP response status, e.g., 200 for "200 OK"
+				var data = JSON.parse(request.responseText); // Returned data, e.g., an HTML document.
+
+				if(status == 200)
+				{
+					successCallback(data);
+				}
+				else
+				{
+					failureCallback(data);
+				}
 			}
-			else
+		}
+		
+		request.open("GET", url, async);
+		request.setRequestHeader("Content-Type", "application/json");
+		request.send();
+	}
+}
+
+function ajaxPost(url,successCallback,failureCallback,requestData,mockResponse) {
+
+	if(mockResponse != null) {
+		console.warn("NOT REAL RESPONSE");
+		successCallback(mockResponse);
+	} else {
+
+		var async = true;
+
+		var request = new XMLHttpRequest();
+
+		request.onreadystatechange  = function() {
+
+			if (request.readyState === 4)
 			{
-				failureCallback(data);
+				var status = request.status; // HTTP response status, e.g., 200 for "200 OK"
+				var data = JSON.parse(request.responseText); // Returned data, e.g., an HTML document.
+
+				if(status == 200)
+				{
+					successCallback(data);
+				}
+				else
+				{
+					failureCallback(data);
+				}
 			}
 		}
 		
 		request.open("POST", url, async);
-		request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+		request.setRequestHeader("Content-Type", "application/json");
+
+		requestData = JSON.stringify(requestData);
 		request.send(requestData);
 	}
 }
