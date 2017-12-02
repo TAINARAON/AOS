ca4a4dd315ab28ee078a8a6d9569e93676063956
Tiaan Gerber <tiaangerberdev@gmail.com>
2017-12-02 15:01:04 +0200

    Tiaan - dateTime only shows date

--- js/common/util.js
+++ js/common/util.js
@@ -121,16 +121,16 @@ var util = new function() {
 	    var year    = now.getFullYear();
 	    var month   = now.getMonth()+1; 
 	    var day     = now.getDate();
-	    var hour    = now.getHours();
+	    /*var hour    = now.getHours();
 	    var minute  = now.getMinutes();
-	    var second  = now.getSeconds(); 
+	    var second  = now.getSeconds(); */
 	    if(month.toString().length == 1) {
 	        var month = '0'+month;
 	    }
 	    if(day.toString().length == 1) {
 	        var day = '0'+day;
 	    }   
-	    if(hour.toString().length == 1) {
+	    /*if(hour.toString().length == 1) {
 	        var hour = '0'+hour;
 	    }
 	    if(minute.toString().length == 1) {
@@ -138,8 +138,8 @@ var util = new function() {
 	    }
 	    if(second.toString().length == 1) {
 	        var second = '0'+second;
-	    }   
-	    var dateTime = year+'/'+month+'/'+day+' '+hour+':'+minute+':'+second;   
+	    }   */
+	    var dateTime = year+'/'+month+'/'+day/*+' '+hour+':'+minute+':'+second*/;   
 	     return dateTime;
 	}
 
